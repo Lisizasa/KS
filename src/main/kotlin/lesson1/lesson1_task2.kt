34559fe1cd2a669120e7300314e710802e72c426
User <lisiz1441@gmail.com>
2025-09-09 19:31:44 +0300

    KS-1-1 update variables

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package lesson1
+
+fun main() {
+    var numberOfOrders: Int = 0
+    val gratitudeForOrder: String = "Thank you for your order!"
+
+    println(numberOfOrders)
+    println(gratitudeForOrder)
+
+    var numberOfWorkers: Int = 2000
+    //    println(numberOfWorkers)
+    numberOfWorkers -= 1
+    println(numberOfWorkers)
+}
