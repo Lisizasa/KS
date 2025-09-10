dba42628adb5fcaad6777f7610f1fd2f95631528
User <lisiz1441@gmail.com>
2025-09-10 17:38:06 +0300

    KS-2-4 done

---
+++ src/main/kotlin/lesson2/lesson2-4.kt
@@ -0,0 +1,9 @@
+package lesson2
+
+fun main() {
+    val crystals: Int = 7
+    val iron: Int = 11
+
+    println("Кристаллическая руда: ${(crystals * 0.2).toInt()}")
+    println("Железная руда: ${(iron * 0.2).toInt()}")
+}
