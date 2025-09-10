976f28165b125c94df38d0d6ca2f879178feb03c
User <lisiz1441@gmail.com>
2025-09-10 15:03:05 +0300

    KS-1-3 done

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,20 @@
+package lesson1
+
+import kotlin.math.min
+
+fun main() {
+    val year: Int = 1961
+    var hour: Int
+    var minute: Int
+
+    hour = 9
+    minute = 7
+
+    println("year = $year")
+    println("hour = $hour")
+    println("minute = $minute")
+
+    hour = 10
+    minute = 55
+    println("$hour:$minute")
+}
