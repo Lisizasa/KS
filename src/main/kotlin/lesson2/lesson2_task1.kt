2dd1044c4864fb5938632ec0e2426dcd6ee1f4b3
User <lisiz1441@gmail.com>
2025-09-11 17:53:43 +0300

    KS-2-1 deleted magic number and file

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -1,7 +1,8 @@
 package lesson2
 
 fun main() {
+    val numberOfStudents: Int = 4
     val allMarks: IntArray = intArrayOf(3, 4, 3, 5)
-    val average: Double = allMarks.sum().toDouble() / 4
+    val average: Double = allMarks.sum().toDouble() / numberOfStudents
     println("%.2f".format(average))
 }
