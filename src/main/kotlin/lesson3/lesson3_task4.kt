7c485842bdb354bdd0b6e2cd18f815b174afb7c0
User <lisiz1441@gmail.com>
2025-09-13 21:21:40 +0300

    KS-3-4 done

---
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -0,0 +1,14 @@
+package lesson3
+
+fun main() {
+    var from: String = "E2"
+    var goal: String = "E4"
+    var number: Int = 1
+
+    println("[$from-$goal;$number]")
+
+    from = "D2"
+    goal = "D3"
+    println("[$from-$goal;$number]")
+    
+}
