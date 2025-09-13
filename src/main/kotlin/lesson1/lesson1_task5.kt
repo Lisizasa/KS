31c561bc7c05eeee7c3a8ca4fead30628549fe41
User <lisiz1441@gmail.com>
2025-09-13 13:40:55 +0300

    KS-2-2 updated constants

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -3,8 +3,10 @@ package lesson1
 fun main() {
     val allSeconds: Short = 6480
     val hours = allSeconds / 3600
-    val minutes = allSeconds/60 % 60
-    val seconds = allSeconds % 60
+    val minutesDivider = 60
+    val secondsDivider = 60
+    val minutes = allSeconds / minutesDivider % minutesDivider
+    val seconds = allSeconds % secondsDivider
 
     println("$hours:$minutes:$seconds")
 }
