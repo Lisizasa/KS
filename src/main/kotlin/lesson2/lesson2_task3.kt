c526fcd6f2de5d1de6d0cbef902ad23768e4d151
User <lisiz1441@gmail.com>
2025-09-13 13:46:39 +0300

    KS-2-2 updated constants

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -1,16 +1,14 @@
 package lesson2
 
 fun main() {
-    val departureHour: Int
-    val departureMinute: Int
-    val travelTime: Int
+    val departureHour: Int = 9
+    val departureMinute: Int = 39
+    val travelTime: Int = 457
+    val hoursDivider = 60
+    val minutesDivider = 60
 
-    departureHour = 9
-    departureMinute = 39
-    travelTime = 457
-
-    val arrivalHour: Int = departureHour + travelTime / 60
-    val arrivalMinute: Int = (departureMinute + travelTime) % 60
+    val arrivalHour: Int = departureHour + travelTime / hoursDivider
+    val arrivalMinute: Int = (departureMinute + travelTime) % minutesDivider
 
     println("Время прибытия: $arrivalHour:$arrivalMinute")
 }
