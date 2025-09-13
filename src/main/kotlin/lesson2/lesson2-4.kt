cb36196351cff7fa56a5612d56e21aa9d691a48c
User <lisiz1441@gmail.com>
2025-09-13 13:48:26 +0300

    KS-2-4 updated constant

--- src/main/kotlin/lesson2/lesson2-4.kt
+++ src/main/kotlin/lesson2/lesson2-4.kt
@@ -3,7 +3,8 @@ package lesson2
 fun main() {
     val crystals: Int = 7
     val iron: Int = 11
+    val percent: Double = 0.2
 
-    println("Кристаллическая руда: ${(crystals * 0.2).toInt()}")
-    println("Железная руда: ${(iron * 0.2).toInt()}")
+    println("Кристаллическая руда: ${(crystals * percent).toInt()}")
+    println("Железная руда: ${(iron * percent).toInt()}")
 }
