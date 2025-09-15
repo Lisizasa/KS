f3717064eba9c6e95923ead89f095f0c0bfb2480
User <lisiz1441@gmail.com>
2025-09-15 14:15:23 +0300

    KS-1-5 made constants

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -2,11 +2,12 @@ package lesson1
 
 fun main() {
     val allSeconds: Short = 6480
-    val hours = allSeconds / 3600
-    val minutesDivider = 60
-    val secondsDivider = 60
-    val minutes = allSeconds / minutesDivider % minutesDivider
-    val seconds = allSeconds % secondsDivider
+    val hours = allSeconds / MINUTES_IN_HOUR / SECONDS_IN_MINUTE
+
+    val minutes = allSeconds / MINUTES_IN_HOUR % MINUTES_IN_HOUR
+    val seconds = allSeconds % SECONDS_IN_MINUTE
 
     println("$hours:$minutes:$seconds")
 }
+const val MINUTES_IN_HOUR = 60
+const val SECONDS_IN_MINUTE = 60
