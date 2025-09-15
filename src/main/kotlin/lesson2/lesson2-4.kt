ca8e2f194ff1984d6e995e534c7df232f71fcb0d
User <lisiz1441@gmail.com>
2025-09-15 14:39:26 +0300

    KS-2-4 fixed percent

--- src/main/kotlin/lesson2/lesson2-4.kt
+++ src/main/kotlin/lesson2/lesson2-4.kt
@@ -3,8 +3,9 @@ package lesson2
 fun main() {
     val crystals: Int = 7
     val iron: Int = 11
-    val percent: Double = 0.2
+    val percent: Double = 20.0
 
-    println("Кристаллическая руда: ${(crystals * percent).toInt()}")
-    println("Железная руда: ${(iron * percent).toInt()}")
+    println("Кристаллическая руда: ${(crystals * (percent / PERCENT_DIVIDER)).toInt()}")
+    println("Железная руда: ${(iron * (percent / PERCENT_DIVIDER)).toInt()}")
 }
+const val PERCENT_DIVIDER: Double = 100.0
